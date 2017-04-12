15a09438620ef4e04a9a6ba8699f7a1a31b8e882
duza <duza@tut.by>
2017-04-12 20:43:32 +0300

    Added opportunity toggle Warning component with help return null

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -3,6 +3,43 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 //import './index.css';
 
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null;
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  );
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {showWarning: true}
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
+    this.setState(prevState => ({
+      showWarning: !prevState.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    );
+  }
+}
+
 function Mailbox(props) {
   const unreadMessages = props.unreadMessages;
   return (
@@ -101,6 +138,7 @@ function App(){
     <Toggle />
     <Clock />
     <Mailbox unreadMessages={messages} />
+    <Page /> 
     </div>
   );
 }
