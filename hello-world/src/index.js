34d6381b9607b0b98d3082680a80ef644d47eb7f
duza <duza@tut.by>
2017-04-12 19:48:06 +0300

    Added LoginControl component and bound it with Greeting and Login(out)Buttons

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -3,6 +3,57 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 //import './index.css';
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick(){
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick(){
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let button = null;
+    if (isLoggedIn) {
+        button = <LogoutButton onClick={this.handleLogoutClick} />;
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />;
+    }
+  
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    );
+  }
+}
+
 function UserGreeting(props) {
   return <h1>Welcome back!</h1>;
 }
@@ -23,7 +74,7 @@ function Greeting(props) {
 function App(){
   return (
     <div>
-    <Greeting isLoggedIn={true} />
+    <LoginControl />
     <Clock />
     <Clock />
     <Toggle />
