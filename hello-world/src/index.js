4a7ff02dc975600dff21717a8941761595203244
duza <duza@tut.by>
2017-04-12 18:29:18 +0300

    Changed index. Added Add, Toggle and FormattedDate components

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -2,6 +2,47 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //import App from './App';
 //import './index.css';
+
+function App(){
+  return (
+    <div>
+    <Clock />
+    <Clock />
+    <Toggle />
+    <Clock />
+    </div>
+  );
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};
+
+    //this binding is necessary to make 'this' work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log(this);
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }));
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    );
+  }
+}  
+
+function FormattedDate(props){
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>;
+}
+
 class Clock extends React.Component {
   constructor(props) {
      super(props);
@@ -22,14 +63,15 @@ class Clock extends React.Component {
   tick() {
     this.setState({
 	date: new Date()
-    });
+    /*or (prevState, props) => ({ counter: prevState.counter + props.increment*/
+   });
 }
 
   render() {
     return (
    	<div>
 	    <h1>Hello, world!</h1>	
-	    <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+	    <FormattedDate date={this.state.date} />
 	</div>
     );
   }
@@ -37,6 +79,6 @@ class Clock extends React.Component {
 
 
 ReactDOM.render(
-    <Clock />,
+    <App />,
     document.getElementById('root')
 );
