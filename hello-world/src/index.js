888cde55bd4167d43b77ab068eb224c0d7945e8e
duza <duza@tut.by>
2017-04-13 19:56:23 +0300

    Changed calculator according react down flow.

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -3,6 +3,52 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 //import './index.css';
 
+const scaleNames = {
+  c: 'Celsius',
+  f: 'Fahrenheit'
+};
+
+function toCelsius(fahrenheit) {
+  return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9 / 5) + 32;
+}
+
+function tryConvert(value, convert) {
+  const input = parseFloat(value);
+  if (Number.isNaN(input)) {
+    return '';
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+// Look here!!!!
+  handleChange(e) {
+    this.props.onChange(e.target.value);
+  }
+
+  render() {
+    const value = this.props.value;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={value}
+               onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
+
 function BoilingVerdict(props) {
   if (props.celsius >= 100) {
     return <p>The water would boil.</p>;
@@ -13,25 +59,38 @@ function BoilingVerdict(props) {
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {value: ''};
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = {value: '', scale: 'c'};
   }
 
-  handleChange(e) {
-    this.setState({value: e.target.value});
+  handleCelsiusChange(value) {
+    this.setState({scale: 'c', value});
+  }
+
+  handleFahrenheitChange(value) {
+    this.setState({scale: 'f', value});
   }
 
   render() {
+    const scale = this.state.scale;
     const value = this.state.value;
+    const celsius = scale === 'f' ? tryConvert(value, toCelsius) : value;
+    const fahrenheit = scale === 'c' ? tryConvert(value, toFahrenheit) : value;
+
     return (
-      <fieldset>
-        <legend>Enter temperature in Celsius:</legend>
-        <input
-          value={value}
-          onChange={this.handleChange} />
+      <div>
+        <TemperatureInput 
+           scale="c" 
+           value={celsius}
+           onChange={this.handleCelsiusChange} />
+        <TemperatureInput 
+           scale="f"
+           value={fahrenheit}
+           onChange={this.handleFahrenheitChange} />
         <BoilingVerdict
-          celsius={parseFloat(value)} />
-      </fieldset>
+           celsius={parseFloat(celsius)} />
+      </div>
     );
   }
 }
