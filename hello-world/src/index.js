4cdc21419f74573fc97da95bb05592fba15bf58a
duza <duza@tut.by>
2017-04-13 18:55:53 +0300

    Added calculator which display that water would boil or not.

--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 //import './index.css';
 
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {value: ''};
+  }
+
+  handleChange(e) {
+    this.setState({value: e.target.value});
+  }
+
+  render() {
+    const value = this.state.value;
+    return (
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input
+          value={value}
+          onChange={this.handleChange} />
+        <BoilingVerdict
+          celsius={parseFloat(value)} />
+      </fieldset>
+    );
+  }
+}
+
 class FormCheckbox extends React.Component {
   constructor(props) {
     super(props);
@@ -412,6 +445,7 @@ function App(){
     <LoginControl />
     <Blog posts={posts} />
     <Clock />
+    <Calculator />
     <NumberList numbers={numbers} />
     <Clock />
     <Toggle />
